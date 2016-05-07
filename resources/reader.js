2639eb45be27e03fbb82a05ac150b5365e6bb66d
Vasilij Schneidermann <v.schneidermann@gmail.com>
2016-05-07 16:51:03 +0200

    Permit quitting with q or <escape>

--- resources/reader.js
+++ resources/reader.js
@@ -3,4 +3,12 @@ function init() {
     frame.src = window.chicken.documents()[0];
 }
 
+function keyHandler(e) {
+    if (e.keyCode === 81 || e.keyCode === 27 ) { // ASCII Q/ESC
+        e.preventDefault();
+        window.chicken.quit();
+    }
+}
+
 window.addEventListener('load', init);
+window.addEventListener('keydown', keyHandler);
