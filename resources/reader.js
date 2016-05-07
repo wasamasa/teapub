4c72b486116853e0ff425bd6c7a50996a22eebc0
Vasilij Schneidermann <v.schneidermann@gmail.com>
2016-05-07 18:27:53 +0200

    Add page up/down navigation

--- resources/reader.js
+++ resources/reader.js
@@ -7,21 +7,65 @@ function init() {
     frame.src = documents[index];
 }
 
+function prevDocument() {
+    if (index > 0) {
+        index--;
+        frame.src = documents[index];
+    }
+}
+
+function nextDocument() {
+    if (index < documents.length - 1) {
+        index++;
+        frame.src = documents[index];
+    }
+}
+
+function pageUp() {
+    var inner = frame.contentDocument.documentElement;
+    if (inner.scrollTop > 0) {
+        var scrollTop = Math.max(inner.scrollTop - inner.clientHeight, 0);
+        inner.scrollTop = scrollTop;
+    }
+}
+
+function pageDown() {
+    var inner = frame.contentDocument.documentElement;
+    if (inner.scrollTop < inner.scrollHeight - inner.clientHeight) {
+        var scrollTop = Math.min(inner.scrollTop + inner.clientHeight,
+                                 inner.scrollHeight);
+        inner.scrollTop = scrollTop;
+    }
+}
+
+function pageDownOrNextDocument() {
+    var inner = frame.contentDocument.documentElement;
+    if (inner.scrollTop < inner.scrollHeight - inner.clientHeight) {
+        var scrollTop = Math.min(inner.scrollTop + inner.clientHeight,
+                                 inner.scrollHeight);
+        inner.scrollTop = scrollTop;
+    } else {
+        nextDocument();
+    }
+}
+
 function keyHandler(e) {
-    console.log(e.keyCode);
     if (e.keyCode === 81 || e.keyCode === 27 ) { // Q / ESC
-        e.preventDefault();
         window.chicken.quit();
+    } else if (e.keyCode === 32 ) { // SPC
+        if (e.shiftKey) {
+            pageUp();
+        } else {
+            pageDownOrNextDocument();
+        }
+    } else if (e.keyCode === 33) { // <pgup>
+        pageUp();
+    } else if (e.keyCode === 34) { // <pgdn>
+        pageDown();
     } else if (e.keyCode === 80) { // P
-        if (index > 0) {
-            index--;
-            frame.src = documents[index];
-        }
+        prevDocument();
     } else if (e.keyCode === 78) { // N
-        if (index < documents.length - 1) {
-            index++;
-            frame.src = documents[index];
-        }
+        nextDocument();
     }
 }
 
