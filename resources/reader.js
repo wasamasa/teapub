f2556e21cd338a55a0dd5fe138a0d2629050c2bf
Vasilij Schneidermann <v.schneidermann@gmail.com>
2016-05-07 17:49:00 +0200

    Add prev/next navigation

--- resources/reader.js
+++ resources/reader.js
@@ -1,12 +1,27 @@
+var documents, index, frame;
+
 function init() {
     frame = document.getElementById('content');
-    frame.src = window.chicken.documents()[0];
+    documents = window.chicken.documents();
+    index = 0;
+    frame.src = documents[index];
 }
 
 function keyHandler(e) {
-    if (e.keyCode === 81 || e.keyCode === 27 ) { // ASCII Q/ESC
+    console.log(e.keyCode);
+    if (e.keyCode === 81 || e.keyCode === 27 ) { // Q / ESC
         e.preventDefault();
         window.chicken.quit();
+    } else if (e.keyCode === 80) { // P
+        if (index > 0) {
+            index--;
+            frame.src = documents[index];
+        }
+    } else if (e.keyCode === 78) { // N
+        if (index < documents.length - 1) {
+            index++;
+            frame.src = documents[index];
+        }
     }
 }
 
